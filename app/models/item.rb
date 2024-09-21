0b093c6b0bb8ccacedd1bd87460f12703f12e9fa
asanebou.mackey <asanebou.job4219@gmail.com>
2024-09-21 12:59:39 +0900

    ソート機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,13 @@ class Item < ApplicationRecord
   has_many :carts
   has_many :order_details
   has_many :favorites
+  has_many :favorited_users, through: :favorites, source: :user
+  
+  scope :latest, -> { order(created_at: :desc) }
+  scope :old, -> { order(created_at: :asc) }
+  scope :expensive, -> {order(price: :desc)}
+  scope :cheap, -> {order(price: :asc)}
+  scope :most_favorited, -> { includes(:favorited_users).sort_by { |x| x.favorited_users.includes(:favorites).size }.reverse }
 
   def favorited_by(user)
     Favorite.find_by(user_id: user.id, item_id: id)
